f52a91d4a3e72c4103a7e10cf658be18acf2cab3
abhiishekpanchal <abhishekp.280403@gmail.com>
2025-04-13 17:33:03 +0530

    updated the build command

--- server/index.js
+++ server/index.js
@@ -42,10 +42,10 @@ app.use('/server/orderDetails', orderRouter)
 app.use('/server/userDetails', userRouter)
 app.use('/server/inventoryDetails', inventoryRouter)
 
-app.use(express.static(path.join(__dirname, '/client/dist')))
+app.use(express.static(path.join(__dirname, '../client/dist')))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+  res.sendFile(path.join(__dirname, '../client', 'dist', 'index.html'))
 })
 
 app.listen(3000, () => {
