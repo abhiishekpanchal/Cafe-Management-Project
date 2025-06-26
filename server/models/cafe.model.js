eee7297a9acc5aaf68769ed6d7d19073df0620d0
Pradnya Kumbhar <kumbharpradnyaa10@gmail.com>
2025-06-26 17:29:08 +0530

    removed gst validations

--- server/models/cafe.model.js
+++ server/models/cafe.model.js
@@ -111,14 +111,14 @@ const cafeSchema = new mongoose.Schema(
       type: String,
       required: true,
       unique: true,
-      validate: {
-        validator: function (v) {
-          return /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/.test(
-            v
-          );
-        },
-        message: (props) => `${props.value} is not a valid GST number!`,
-      },
+      // validate: {
+      //   validator: function (v) {
+      //     return /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/.test(
+      //       v
+      //     );
+      //   },
+      //   message: (props) => `${props.value} is not a valid GST number!`,
+      // },
     },
   },
   { timestamps: true }
