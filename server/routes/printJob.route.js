19f2d8acd48ae86b3e0aa2a5d736c6e01ce3da3c
adhiraj369 <adhirajsingh369@gmail.com>
2025-07-06 20:45:57 +0530

    Print

--- server/routes/printJob.route.js
+++ server/routes/printJob.route.js
@@ -2,7 +2,10 @@ import express from 'express';
 import { createPrintJob, getPendingPrintJob, markPrintJobStatus } from '../controllers/order.controller.js';
 
 const router = express.Router();
-router.post('/create-job', createPrintJob);
+router.get('/test', (req, res) => {
+  res.send("✅ PrintJob route file is working");
+});
+
 router.get('/get-print-job', getPendingPrintJob);
 router.post('/mark-job-printed', markPrintJobStatus);
 
